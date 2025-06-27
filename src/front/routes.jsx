a36c589d51c3a9d2f2afe4e41264c1ea7677bb79
Juan Fernandez <stormblazart@gmail.com>
2025-06-27 18:31:58 -0400

    Update routes.jsx

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -1,4 +1,4 @@
-// Import necessary components and functions from react-router-dom.
+// src/front/routes.jsx - Updated with authentication routes
 
 import {
     createBrowserRouter,
@@ -9,22 +9,52 @@ import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
 import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
+import { SignUp } from "./pages/SignUp";
+import { Login } from "./pages/Login";
+import { Dashboard } from "./pages/Dashboard"; // You'll need to create this
+import { Profile } from "./pages/Profile"; // You'll need to create this
+import { ProtectedRoute } from "./components/ProtectedRoute"; // You'll need to create this
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
-    // CreateRoutesFromElements function allows you to build route elements declaratively.
-    // Create your routes here, if you want to keep the Navbar and Footer in all views, add your new routes inside the containing Route.
-    // Root, on the contrary, create a sister Route, if you have doubts, try it!
-    // Note: keep in mind that errorElement will be the default page when you don't get a route, customize that page to make your project more attractive.
-    // Note: The child paths of the Layout element replace the Outlet component with the elements contained in the "element" attribute of these child paths.
-
-      // Root Route: All navigation will start from here.
-      <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >
-
-        {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
-        <Route path= "/" element={<Home />} />
-        <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
-        <Route path="/demo" element={<Demo />} />
-      </Route>
+        // Root Route: All navigation will start from here
+        <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>}>
+            
+            {/* Public Routes */}
+            <Route path="/" element={<Home />} />
+            <Route path="/demo" element={<Demo />} />
+            <Route path="/single/:theId" element={<Single />} />
+            
+            {/* Authentication Routes */}
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            
+            {/* Protected Routes - Require Authentication */}
+            <Route path="/dashboard" element={
+                <ProtectedRoute>
+                    <Dashboard />
+                </ProtectedRoute>
+            } />
+            
+            <Route path="/profile" element={
+                <ProtectedRoute>
+                    <Profile />
+                </ProtectedRoute>
+            } />
+            
+            {/* Gaming Routes - Future implementation */}
+            <Route path="/sessions" element={
+                <ProtectedRoute>
+                    <div>Gaming Sessions Page</div>
+                </ProtectedRoute>
+            } />
+            
+            <Route path="/friends" element={
+                <ProtectedRoute>
+                    <div>Friends Page</div>
+                </ProtectedRoute>
+            } />
+            
+        </Route>
     )
 );
