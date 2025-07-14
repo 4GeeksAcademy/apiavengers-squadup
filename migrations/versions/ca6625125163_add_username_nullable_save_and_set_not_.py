ceea2c1de1f1c9896e278a0dedf70a237ea3062a
Dabeldab <1dareios@gmail.com>
2025-07-14 23:55:06 +0000

    migration spaghetti

--- migrations/versions/ca6625125163_add_username_nullable_save_and_set_not_.py
+++ migrations/versions/ca6625125163_add_username_nullable_save_and_set_not_.py
@@ -19,15 +19,8 @@ depends_on = None
 
 def upgrade():
 
-    # Step 1 – add with default & NOT NULL
-    op.add_column(
-        'user',
-        sa.Column('username', sa.String(80),
-                nullable=False,
-                server_default='temp_user')
-    )
+    
 
-    op.add_column('user', sa.Column('password_hash', sa.String(200), nullable=True))
     op.execute(f"UPDATE \"user\" SET password_hash = '{temp}'")
     op.alter_column('user', 'password_hash', nullable=False)
 
